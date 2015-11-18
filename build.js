cf78e29cbfc89649dcee9ca52348d0a15bde0edd
schisma <jona@schisma.co>
2015-11-18 18:39:45 +0100

    add some docs

--- build.js
+++ build.js
@@ -4,6 +4,8 @@ const MSass = require('metalsmith-sass')
 
 const path = require('path')
 
+// The core pipeline. This is the portion of the page you see when visiting the
+// index. It's basically just Jade files.
 const core = Metalsmith(path.join(__dirname, 'core'))
   .source('./')
   .destination('../build')
@@ -14,6 +16,9 @@ const core = Metalsmith(path.join(__dirname, 'core'))
     if (err) { throw new Error(err) }
   })
 
+// The styles pipeline. I need a separate one because the styles are global,
+// they're not pipeline-specific. Bundles up SCSS files and pipes them (or IT)
+// into ./build/css.
 const styles = Metalsmith(path.join(__dirname, 'styles'))
   .source('./')
   .destination('../build/css')
