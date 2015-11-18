b23cb40f0a60eac7e7f4d2da5539a95858b1796f
schisma <jona@schisma.co>
2015-11-18 18:31:30 +0100

    add styling with scss

--- build.js
+++ build.js
@@ -1,5 +1,6 @@
 const Metalsmith = require('metalsmith')
 const MJade = require('metalsmith-jade')
+const MSass = require('metalsmith-sass')
 
 const path = require('path')
 
@@ -9,6 +10,14 @@ const core = Metalsmith(path.join(__dirname, 'core'))
   .use(MJade({
     pretty: true
   }))
+  .build(err => {
+    if (err) { throw new Error(err) }
+  })
+
+const styles = Metalsmith(path.join(__dirname, 'styles'))
+  .source('./')
+  .destination('../build/css')
+  .use(MSass())
   .build(err => {
     if (err) { throw new Error(err) }
   })
